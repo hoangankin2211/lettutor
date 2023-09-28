b430d0e2b1e90ae8953745fe4e0b083b36e7c1f9
hoangankin2211 <truonghuynhduchoang@gmail.com>
2023-09-28 23:14:49 +0700

    setting up project environment

---
+++ lib/ui/shared/app_setting/bloc/app_settings_bloc.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:injectable/injectable.dart';
+
+part 'app_settings_event.dart';
+
+part 'app_settings_state.dart';
+
+@singleton
+class AppSettingsBloc extends Bloc<AppSettingsEvent, AppSettingsState> {
+  AppSettingsBloc()
+      : super(AppSettingsInitial(AppSettingModel(
+          clipboardData: <String>[],
+          isHideWindow: false,
+        )));
+
+// FutureOr<void> _onInitAppSettingsEvent(
+//   InitAppSettingsEvent event,
+//   Emitter<AppSettingsState> emit,
+// ) {
+//   // emit(AppSettingsInitial(AppSettingModel(
+//   //   clipboardData: listData ?? <String>[],
+//   //   gptToken: gptToken,
+//   //   shortcutMenu: shortcutMenu,
+//   //   isHideWindow: isHideWindow,
+//   // )));
+// }
+}
